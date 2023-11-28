a1987a2e5e43cfb37bbf93403add4d67dc35b59c
Kari Vigna <karivigna24@gmail.com>
2023-11-27 20:12:32 -0800

    clientside runs with api

--- GreenHarborClient/Models/ApiHelper.cs
+++ GreenHarborClient/Models/ApiHelper.cs
@@ -8,25 +8,25 @@ namespace GreenHarborClient.Models
     public static async Task<string> GetAll()
     {
       RestClient client = new RestClient("https://localhost:7099/");
-      RestRequest request = new RestRequest($"api/Composts", Method.Get);
+      RestRequest request = new RestRequest($"api/composts", Method.Get);
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
 
-     public static async Task<string> Get(int id)
+    public static async Task<string> Get(int id)
     {
       RestClient client = new RestClient("https://localhost:7099/");
       // may need to change
-      RestRequest request = new RestRequest($"api/Composts/{id}", Method.Get);
+      RestRequest request = new RestRequest($"api/composts/{id}", Method.Get);
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
 
-     public static async void Post(string newCompost)
+    public static async void Post(string newCompost)
     {
       RestClient client = new RestClient("https://localhost:7099/");
       // may need to change
-      RestRequest request = new RestRequest($"api/Composts", Method.Post);
+      RestRequest request = new RestRequest($"api/composts", Method.Post);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newCompost);
       await client.PostAsync(request);
@@ -36,7 +36,7 @@ namespace GreenHarborClient.Models
     {
       RestClient client = new RestClient("https://localhost:7099/");
       // may need to change
-      RestRequest request = new RestRequest($"api/Composts/{id}", Method.Put);
+      RestRequest request = new RestRequest($"api/composts/{id}", Method.Put);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newCompost);
       await client.PutAsync(request);
@@ -45,7 +45,7 @@ namespace GreenHarborClient.Models
     public static async void Delete(int id)
     {
       RestClient client = new RestClient("https://localhost:7099/");
-      RestRequest request = new RestRequest($"api/Composts/{id}", Method.Delete);
+      RestRequest request = new RestRequest($"api/composts/{id}", Method.Delete);
       request.AddHeader("Content-Type", "application/json");
       await client.DeleteAsync(request);
     }
